059ecec7d8481586d36ac86311e55717272a2629
jennafritz <jennafritz24@gmail.com>
2021-04-25 18:34:57 -0400

    commit tree c06f3200d5e33cf00dc36464f1613da73c6d1d54

--- index.js
+++ index.js
@@ -4,16 +4,23 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, callback) {
+      let valueArray = Object.values(collection)
+      valueArray.forEach((element, key, collection) => {
+        return callback(element, key, collection)})
+      return collection
     },
 
-    map: function() {
-
+    map: function(collection, callback) {
+      let valueArray = Object.values(collection)
+      let mappedArray = valueArray.map(callback)
+      return mappedArray
     },
 
-    reduce: function() {
-
+    reduce: function(collection, callback, acc = "") {
+      let valueArray = Object.values(collection)
+      let reducedValue = valueArray.reduce((acc, val, collection) => callback(acc, val, collection))
+      return reducedValue
     },
 
     functions: function() {
